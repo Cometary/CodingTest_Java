65fb8f143c239fd81cd6513252071a48601e0dc4
Cometary <109892844+Cometary@users.noreply.github.com>
2023-09-08 13:32:10 +0900

    [unrated] Title: 문자열로 변환, Time: 0.06 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181845. 문자열로 변환/문자열로 변환.java
+++ 프로그래머스/unrated/181845. 문자열로 변환/문자열로 변환.java
@@ -1,6 +1,11 @@
 class Solution {
     public String solution(int n) {
+        // 0.03ms
         String answer = Integer.toString(n);
+        // 0.03ms
+        // String answer = String.valueOf(n);
+        // 10ms
+        // String answer = ""+n;
         return answer;
     }
 }
