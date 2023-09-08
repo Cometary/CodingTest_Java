d955783f97013876dd70b7f895d9cacd90008569
Cometary <109892844+Cometary@users.noreply.github.com>
2023-09-08 14:14:21 +0900

    [unrated] Title: 문자열 곱하기, Time: 0.04 ms, Memory: 71.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.java
+++ 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.java
@@ -1,9 +1,13 @@
 class Solution {
     public String solution(String my_string, int k) {
         String answer = "";
-        for(int i=0; i<k; i++){
-            answer += my_string;
-        }
+        // 1ms
+        // for(int i=0; i<k; i++){
+        //     answer += my_string;
+        // }
+        
+        answer = my_string.repeat(k);
+        
         return answer;
     }
 }
