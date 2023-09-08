20295580810f226138444c0859adde1ac5b0f491
Cometary <109892844+Cometary@users.noreply.github.com>
2023-09-08 13:46:07 +0900

    [unrated] Title: 이어 붙인 수, Time: 0.02 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.java
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.java
@@ -1,16 +1,30 @@
 class Solution {
     public int solution(int[] num_list) {
         int answer = 0;
-        String odd = "";
-        String even = "";
-        for(int num : num_list){
-            if(num%2==0){
+        // 10ms
+        // String odd = "";
+        // String even = "";
+        // for(int num : num_list){
+        //     if(num%2==0){
+        //         even += num;
+        //     }else{
+        //         odd += num;
+        //     }
+        // }
+        // answer = Integer.parseInt(odd)+Integer.parseInt(even);
+        int even = 0;
+        int odd = 0;
+
+        for(int num : num_list) {
+            if(num % 2 == 0) {
+                even *= 10;
                 even += num;
-            }else{
+            } else {
+                odd *= 10;
                 odd += num;
             }
         }
-        answer = Integer.parseInt(odd)+Integer.parseInt(even);
+        answer = even + odd;
         return answer;
     }
 }
