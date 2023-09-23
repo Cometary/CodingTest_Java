718072585f35249dd550165effaa4e6514c3b58e
Cometary <109892844+Cometary@users.noreply.github.com>
2023-09-23 21:24:16 +0900

    [unrated] Title: 0 떼기, Time: 0.02 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String n_str) {
+        int i_str = Integer.parseInt(n_str);
+        String answer = Integer.toString(i_str);
+        return answer;
+    }
+}
