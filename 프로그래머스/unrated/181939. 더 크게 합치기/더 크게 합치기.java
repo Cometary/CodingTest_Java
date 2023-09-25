86a1411aa25419fa0dd6ce835b11ee2a492e9a21
Cometary <109892844+Cometary@users.noreply.github.com>
2023-09-25 21:03:26 +0900

    [unrated] Title: 더 크게 합치기, Time: 8.62 ms, Memory: 70.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        String ab = ""+a+b;
+        String ba = ""+b+a;
+        answer = Integer.parseInt(ab)>Integer.parseInt(ba) ? Integer.parseInt(ab) : Integer.parseInt(ba);
+        return answer;
+    }
+}
