3dc8c161fc2db123dc0d006127f7dba489d4ecea
Cometary <109892844+Cometary@users.noreply.github.com>
2023-11-04 22:21:00 +0900

    [level unrated] Title: 배열 만들기 3, Time: 0.11 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] intervals) {
+        int intSize = intervals[0][1]+intervals[1][1]-intervals[0][0]-intervals[1][0]+2;
+        int[] answer = new int[intSize];
+        int index = 0;
+        for(int i =intervals[0][0]; i<=intervals[0][1]; i++){
+            answer[index]=arr[i];
+            index++;
+        }
+        for(int i =intervals[1][0]; i<=intervals[1][1]; i++){
+            answer[index]=arr[i];
+            index++;
+        }
+        return answer;
+    }
+}
