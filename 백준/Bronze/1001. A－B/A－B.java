dc568a9fb9e199e364f7238ac74e8ee59de1487a
Cometary <109892844+Cometary@users.noreply.github.com>
2024-07-19 09:13:38 +0900

    [Bronze V] Title: A-B, Time: 124 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String[] ab = br.readLine().split(" ");
+        
+        int a = Integer.parseInt(ab[0]);
+        int b = Integer.parseInt(ab[1]);
+        
+        System.out.println(a-b);
+    }
+}
