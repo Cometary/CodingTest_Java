b8f937ed821753974616c26a913c1fc7c129bba2
Cometary <109892844+Cometary@users.noreply.github.com>
2024-09-24 11:31:44 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 164 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        String s = sc.nextLine();
+        
+        int a = Integer.parseInt(s);
+        
+        int b = 2541-1998;
+        
+        System.out.println(a-b);
+        
+    }
+}
